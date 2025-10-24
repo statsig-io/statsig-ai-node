a8895fddbe0c0088fd03a29706fdc533db8a4211
Alisha Lakkad <alishalakkad@MacBook-Pro-2.local>
2025-10-24 15:17:36 -0700

    added testing for targeting

--- src/__tests__/prompt.test.ts
+++ src/__tests__/prompt.test.ts
@@ -52,11 +52,11 @@ describe('Prompt Serving', () => {
     statsigAI = new StatsigAI(sdkKey, statsig);
     await statsigAI.initialize();
     const user = new StatsigUser({
-      userID: 'test-user',
+      userID: 'test-user-1234',
     });
-    const prompt = statsigAI.getPrompt(user, 'test_prompt');
+    const prompt = statsigAI.getPrompt(user, 'test-prompt-1');
     expect(prompt).toBeDefined();
-    expect(prompt.getName()).toBe('test_prompt');
+    expect(prompt.getName()).toBe('test-prompt-1');
   });
 
   it('should get the correct live prompt version', async () => {
@@ -65,12 +65,12 @@ describe('Prompt Serving', () => {
     statsigAI = new StatsigAI(sdkKey, statsig);
     await statsigAI.initialize();
     const user = new StatsigUser({
-      userID: 'test-user',
+      userID: 'test-user-1234',
     });
-    const prompt = statsigAI.getPrompt(user, 'test_prompt');
+    const prompt = statsigAI.getPrompt(user, 'test-prompt-1');
     const liveVersion = prompt.getLive();
     expect(liveVersion).toBeDefined();
-    expect(liveVersion.getID()).toBe('6KGzeo8TR9JTL7CZl7vccd');
+    expect(liveVersion.getID()).toBe('2zMuUzehqVugoL5CaS08xh');
     expect(liveVersion.getName()).toBe('Version 1');
     expect(liveVersion.getTemperature()).toBe(1);
     expect(liveVersion.getMaxTokens()).toBe(1000);
@@ -80,9 +80,61 @@ describe('Prompt Serving', () => {
     expect(liveVersion.getProvider()).toBe('openai');
     expect(liveVersion.getModel()).toBe('gpt-5');
     expect(liveVersion.getWorkflowBody()).toBeDefined();
-    expect(liveVersion.getEvalModel()).toBe('gpt-4o-mini');
+    expect(liveVersion.getEvalModel()).toBe('gpt-5');
     expect(liveVersion.getType()).toBe('Live');
-    expect(liveVersion.getAIConfigName()).toBe('test_prompt');
+    expect(liveVersion.getAIConfigName()).toBe('test-prompt-1');
+  });
+
+  it('should support targeting rules', async () => {
+    statsig = new Statsig(sdkKey, options);
+    await statsig.initialize();
+    statsigAI = new StatsigAI(sdkKey, statsig);
+    await statsigAI.initialize();
+    const user = new StatsigUser({
+      userID: 'test-user',
+    });
+    const prompt = statsigAI.getPrompt(user, 'test-prompt-1');
+    const liveVersion = prompt.getLive();
+    expect(liveVersion).toBeDefined();
+    expect(liveVersion.getID()).toBe('2QGncLi0YSYj9zavJ825qB');
+    expect(liveVersion.getName()).toBe('Version 1');
+    expect(liveVersion.getTemperature()).toBe(0);
+    expect(liveVersion.getMaxTokens()).toBe(1000);
+    expect(liveVersion.getTopP()).toBe(1);
+    expect(liveVersion.getFrequencyPenalty()).toBe(0);
+    expect(liveVersion.getPresencePenalty()).toBe(0);
+    expect(liveVersion.getProvider()).toBe('openai');
+    expect(liveVersion.getModel()).toBe('gpt-4o');
+    expect(liveVersion.getWorkflowBody()).toBeDefined();
+    expect(liveVersion.getEvalModel()).toBe('gpt-5');
+    expect(liveVersion.getType()).toBe('Live');
+    expect(liveVersion.getAIConfigName()).toBe('test-prompt-2');
+  });
+
+  it('should support nested targeting rules', async () => {
+    statsig = new Statsig(sdkKey, options);
+    await statsig.initialize();
+    statsigAI = new StatsigAI(sdkKey, statsig);
+    await statsigAI.initialize();
+    const user = new StatsigUser({
+      userID: 'test-user-1',
+    });
+    const prompt = statsigAI.getPrompt(user, 'test-prompt-1');
+    const liveVersion = prompt.getLive();
+    expect(liveVersion).toBeDefined();
+    expect(liveVersion.getID()).toBe('36qqmfh8wHcKzjmX2My2RQ');
+    expect(liveVersion.getName()).toBe('Version 1');
+    expect(liveVersion.getTemperature()).toBe(2);
+    expect(liveVersion.getMaxTokens()).toBe(1000);
+    expect(liveVersion.getTopP()).toBe(1);
+    expect(liveVersion.getFrequencyPenalty()).toBe(0);
+    expect(liveVersion.getPresencePenalty()).toBe(0);
+    expect(liveVersion.getProvider()).toBe('openai');
+    expect(liveVersion.getModel()).toBe('gpt-4.1');
+    expect(liveVersion.getWorkflowBody()).toBeDefined();
+    expect(liveVersion.getEvalModel()).toBe('gpt-5');
+    expect(liveVersion.getType()).toBe('Live');
+    expect(liveVersion.getAIConfigName()).toBe('test-prompt-3');
   });
 
   it('should get the correct candidate prompt versions', async () => {
@@ -91,17 +143,17 @@ describe('Prompt Serving', () => {
     statsigAI = new StatsigAI(sdkKey, statsig);
     await statsigAI.initialize();
     const user = new StatsigUser({
-      userID: 'test-user',
+      userID: 'test-user-1234',
     });
-    const prompt = statsigAI.getPrompt(user, 'test_prompt');
+    const prompt = statsigAI.getPrompt(user, 'test-prompt-1');
     const candidateVersions = prompt.getCandidates();
     expect(candidateVersions).toBeDefined();
     expect(candidateVersions.length).toBe(2);
 
-    expect(candidateVersions[0].getID()).toBe('7jszgFEAi1KRA2Tot6qikg');
+    expect(candidateVersions[0].getID()).toBe('6p8nIvJsfMW7Awnum7aZk5');
     expect(candidateVersions[0].getName()).toBe('Version 2');
 
-    expect(candidateVersions[1].getID()).toBe('7CKLvQvOwjj2vjx12gFO0Z');
+    expect(candidateVersions[1].getID()).toBe('6tNdowtW2qiWkaUhK1H8UU');
     expect(candidateVersions[1].getName()).toBe('Version 3');
   });
 });
